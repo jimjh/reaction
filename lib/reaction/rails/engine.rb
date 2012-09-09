fe96422bd470dfd6a34f21dc9e6f1ee827a4fccb
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2012-09-09 13:56:58 -0400

    added comments

--- lib/reaction/rails/engine.rb
+++ lib/reaction/rails/engine.rb
@@ -6,7 +6,13 @@ module Reaction
     class Engine < ::Rails::Engine
       initializer 'static_assets.load_static_assets',
         :group => :all do |app|
+        # FIXME: namespacing
+        # When I release the gem, all my javascript files should be compiled
+        # and minified in a single file that the user can use via the asset
+        # pipeline. Vendor scripts e.g. underscore.js, backbone.js, jquery should
+        # not be included, but should be specified as dependencies.
         app.middleware.use ::ActionDispatch::Static, File.join(Reaction.paths.root, '..', 'app')
+        app.middleware.use ::ActionDispatch::Static, File.join(Reaction.paths.root, '..', 'vendor')
       end
     end
 
